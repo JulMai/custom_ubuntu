07874ec66759b1d2c6cb5688134710e5f7eb61c3
JulMai <julian_mai@outlook.de>
2022-01-27 18:45:09 +0100

    added install script for material-shell, not fully tested yet, first tests looked good

---
+++ install_material-shell.sh
@@ -0,0 +1,4 @@
+sudo apt install make nodejs npm
+git clone https://github.com/material-shell/material-shell.git && cd material-shell && make install
+gnome-session-quit
+gnome-extensions enable material-shell@papyelgringo
